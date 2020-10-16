e928baa5b7aff289fec506a7029d091995a434f0
NoFishLikeIan <andreatitton96@gmail.com>
2020-10-16 18:49:57 +0200

    🎉 first commit!

---
+++ src/index.tsx
@@ -0,0 +1,8 @@
+import * as React from 'react';
+import { render } from 'react-dom';
+
+const Application: React.FunctionComponent<{}> = () => (
+  <h1>Application</h1>
+);
+
+render(<Application />, document.getElementById('root'));
