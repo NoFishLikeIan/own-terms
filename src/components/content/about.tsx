d46f87868a76f4938932392c4c9b799c3f492f44
NoFishLikeIan <andreatitton96@gmail.com>
2020-10-23 15:05:03 +0200

    👨‍🎨 styled link

--- src/components/content/about.tsx
+++ src/components/content/about.tsx
@@ -1,11 +1,22 @@
 import * as React from 'react'
 import { ContentProps } from './types'
 
+
+
 export const About: React.FunctionComponent<ContentProps> = () => {
 
     return (
         <div style={{ padding: 20, textAlign: "left" }}>
-            {"Hello my friends. This is me"}
+            <p>
+                Hello!
+            </p>
+            <p>
+                I am just a guy interested in math, biology, and economics.
+            </p>
+            <p>
+                Programmed at <a target="_blank" href="https://github.com/accurat/">Accurat</a>.
+                Ph.D. at <a target="_blank" href="https://www.tinbergen.nl/home">Tinbergen</a>.
+            </p>
         </div >
     )
 }
