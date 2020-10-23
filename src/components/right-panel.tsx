538aa2de31a684ffbe78cb637cbada10187f395f
NoFishLikeIan <andreatitton96@gmail.com>
2020-10-23 15:38:17 +0200

    🦩 boids-ts

--- src/components/right-panel.tsx
+++ src/components/right-panel.tsx
@@ -5,6 +5,7 @@ import { colors, style } from '../constant'
 import { Dimensions } from "../state/useWindowDimensions"
 import { ComponentProps } from './types'
 
+
 interface RightPanelProps extends ComponentProps { }
 
 export const RightPanel: React.FunctionComponent<RightPanelProps> = (props) => {
@@ -16,9 +17,10 @@ export const RightPanel: React.FunctionComponent<RightPanelProps> = (props) => {
             style={{
                 ...dimensions,
                 backgroundColor: colors.lightBlue,
-            }}
-        >
-            Hello!
+                display: "flex",
+                justifyContent: "center",
+                alignItems: "center"
+            }}>
         </div>
     )
 }
